c1493a9f09656dd64c95738db28b10e8ef1cb5fa
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-09-04 18:51:25 +0200

    Fix: Pages structure and Sections created for works

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,55 +1,34 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import BlockContent from '@sanity/block-content-to-react'
 
 import {
   SectContact,
   SectProcess } from '../components/'
 
-const serializers = {
-  types: {
-    code: props => (
-      <pre data-language={props.node.language}>
-        <code>{props.node.code}</code>
-      </pre>
-    )
-  }
-}
+import SectWork from './works/SectWork'
+
 
 class Works extends Component {
 
   componentDidMount() {
     window.scrollTo(0, 0)
   }
+
+  backButtonHandler() {
+    console.log('go back');
+    this.props.history.goBack()
+  }
   render() {
     const {title,subtitle,poster,mainImage,body} = this.props.location.state;
     console.log(this.props.location.state);
     return (
             <div>
-      <section id="about" className="s-about target-section">
 
-          <div className="row section-header bit-narrow aos-init aos-animate" data-aos="fade-up">
-              <div className="col-full">
-                  <h3 className="subhead">
-                  <Link to='/' className="backlink">works </Link> / {this.props.match.params.slug}</h3>
-                  <h1 className="display-1">
-                    {/*poster && <img className="pull-left" src={poster+'?h=160&w=160&fit=min'} />*/}
-                    {subtitle}</h1>
-              </div>
-          </div>
-
-          <div className="row bit-narrow aos-init aos-animate" data-aos="fade-up">
-              <div className="col-full">
-                  <p className="lead"></p>
-                  <BlockContent
-                    blocks={body}
-                    serializers={serializers}
-                  />
-              </div>
-          </div>
-
-      </section>
-
+        <SectWork
+          subtitle={subtitle}
+          body={body}
+          slug={this.props.match.params.slug}
+          goBack={()=>this.backButtonHandler()}
+          />
         <SectProcess />
         <SectContact />
       </div>
